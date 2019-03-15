f54345d16ad558fa8c92673961f5fa951dac4817
Agueda Perez <aguedapm@yahoo.com>
2019-03-15 12:14:54 +0100

    added black background to picture

--- contactPage.js
+++ contactPage.js
@@ -1,11 +1,10 @@
-document.getElementById("myButton").addEventListener("click", messageEnvoi);
-
-function messageEnvoi() {
-  document.getElementById("myForm").style.display = "none";
-  document.getElementById("thankYouMessage").style.display = "block";
-}
 
 function openForm() {
   document.getElementById("myForm").style.display = "block";
   document.getElementById("openButton").style.display = "none";
+}
+
+function messageEnvoi() {
+  document.getElementById("thankYouMessage").style.display = "block";
+  document.getElementById("myForm").style.display = "none";
 }
