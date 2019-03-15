4a7c38a28a80b613e3144a304e6d6c4b18e8ed26
DeandreAR <andry.ramarolahy@gmail.com>
2019-03-15 10:01:15 +0100

    Biography htlm css js

---
+++ Biography.js
@@ -0,0 +1,8 @@
+function myFunction() {
+ 
+const x = document.getElementById("snackbar");
+
+    x.className = "show";
+
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
